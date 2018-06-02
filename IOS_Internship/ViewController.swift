2c627d9ebe9430ee938b125c3c91a9eba7c36841
Galkin <galkinivanv@gmail.com>
2018-06-02 14:10:24 +0300

    add src

---
+++ IOS_Internship/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS_Internship
+//
+//  Created by Иван Галкин on 02.06.2018.
+//  Copyright © 2018 geekbrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
